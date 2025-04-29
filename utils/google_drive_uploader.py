34d8f5bcf956dad18fa43937cd173bc5120e0bec
Lam <LamTrang@abedu.vn>
2025-04-29 10:04:24 +0700

    a

--- utils/google_drive_uploader.py
+++ utils/google_drive_uploader.py
@@ -20,6 +20,7 @@ def upload_file_to_drive(filepath, drive_folder):
         folder = service.files().create(body=file_metadata, fields='id').execute()
         folder_id = folder.get('id')
 
+    print(f"[Drive] Folder ID: {folder_id}")
     file_metadata = {'name': os.path.basename(filepath), 'parents': [folder_id]}
     media = MediaFileUpload(filepath, resumable=True)
     service.files().create(body=file_metadata, media_body=media, fields='id').execute()
